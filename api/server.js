eb77a700658b6cbf5fcf502ced2f58f5c16ab960
ConstanceNwaigwe <constancenwaigwe@gmail.com>
2021-11-21 02:34:17 -0500

    completed server

--- api/server.js
+++ api/server.js
@@ -1,1 +1,18 @@
 // build your server here and require it from index.js
+const express = require('express');
+const server = express();
+const projectRouter = require('../api/project/router');
+const resourceRouter = require('../api/resource/router');
+const taskRouter = require('../api/task/router');
+
+server.use(express.json());
+
+server.use('/api/projects', projectRouter);
+server.use('/api/resources', resourceRouter);
+server.use('/api/tasks', taskRouter);
+
+server.use('*', (req,res) => {
+    res.status(404).json({message: 'not found'})
+});
+
+module.exports = server;
