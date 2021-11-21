ad674ce447c45aeedf27a9376e1501a366da8ae6
ConstanceNwaigwe <constancenwaigwe@gmail.com>
2021-11-21 01:58:18 -0500

    completed models

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,17 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig');
+
+function getAll() {
+    return db('projects');
+};
+
+function create(project) {
+    return db('projects')
+      .insert(project)
+      .then(ids => ({ id: ids[0] }));
+};
+
+module.exports = {
+    getAll,
+    create
+};
